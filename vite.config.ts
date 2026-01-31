fd03d65ae3878ae14445ff61c06fa2773b40d4fd
sonamsaini-01 <sonamsaini045@gmail.com>
2026-01-31 23:35:04 +0530

    Updated project with recent changes

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,25 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: './',
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
+  },
+  server: {
+    host: true,
+    port: 5173,
+    strictPort: true,
+    open: true,
+    cors: true,
+  },
+  preview: {
+    host: true,
+    port: 5173,
+    strictPort: true,
+    open: true,
   },
 })
